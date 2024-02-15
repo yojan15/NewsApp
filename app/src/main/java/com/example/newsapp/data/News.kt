00e355185305287fd0277e26f770227d6c66b229
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-02-15 09:43:46 +0530

    checking in to database

--- app/src/main/java/com/example/newsapp/data/News.kt
+++ app/src/main/java/com/example/newsapp/data/News.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.data
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 data class News (
@@ -9,18 +11,20 @@ data class News (
     val status: String
 )
 @Parcelize
+@Entity
 data class Article(
     val publishedAt: String,
     val urlToImage: String? = null,
     val description: String? = null,
 //    val source: Source,
     val title: String,
+    @PrimaryKey
     val url: String,
     val author: String? = null,
     val content: String? = null
 ) : Parcelable
 
-data class Source(
+data class Source (
     val name: String,
     val id: String? = null
 )
