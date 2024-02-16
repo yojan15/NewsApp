1efbaf09435813d6215ed4c88e4ef277511503a1
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-02-16 12:05:59 +0530

    link added to navigate to full article on webpage

--- app/src/main/java/com/example/newsapp/fragments/FullNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/fragments/FullNewsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.newsapp.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -33,8 +35,18 @@ class FullNewsFragment : Fragment(){
                 .load(article.urlToImage)
                 .placeholder(R.drawable.placeholder_image)
                 .into(binding.fullNewsImageView)
+
+            binding.fullNewsUrl.text = article.url
+            binding.fullNewsUrl.setOnClickListener {
+                openWebPage(article.url)
+            }
         } ?: run {
            Log.e("FullNewsFragment","Null")
         }
+    }
+
+    private fun openWebPage(url : String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
